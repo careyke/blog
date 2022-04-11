01f81c016d9f0c8980831996953fb6646dd08fe4
carey <wutkechuang@163.com>
2022-04-11 23:09:23 +0800

    feat: add docsearch

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -24,7 +24,7 @@ module.exports = {
       {
         apiKey: "f04be3a2f45bdf7185bb6e80b91fef2e",
         indexName: "careyke",
-        // appId: "7HYVRLWMRC",
+        appId: "7HYVRLWMRC",
         locales: {
           '/': {
             placeholder: 'Search Documentation',
